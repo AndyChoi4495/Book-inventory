e0c284eb27285ab813e0a522f38a40b6acd95466
YS <87403433+AndyChoi4495@users.noreply.github.com>
2025-06-23 16:02:33 -0400

    Update api.js

--- Book-frontend/src/services/api.js
+++ Book-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://book-inventory-go7b.onrender.com/5000',
+    baseURL: 'https://book-inventory-go7b.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
