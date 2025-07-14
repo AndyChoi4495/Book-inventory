f1b2ec391848149f34b98885e8ac4e2fa6e2fb63
YS <87403433+AndyChoi4495@users.noreply.github.com>
2025-07-14 14:22:31 -0400

    Update database.js

--- Book-backend/config/database.js
+++ Book-backend/config/database.js
@@ -3,11 +3,15 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Required for most managed Postgres (Heroku, Render, etc)
+    },
+  },
 });
 
 module.exports = sequelize;
