13f84566ebacc57a9d04aa293ddad8e43d983eeb
YS <87403433+AndyChoi4495@users.noreply.github.com>
2025-07-15 09:26:30 -0400

    Update index.js

--- Book-backend/index.js
+++ Book-backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'https://andychoi4495.github.io' }));
 app.use(morgan('dev'));
 
 // Test Route
