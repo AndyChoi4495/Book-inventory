38fb4c39b3da828fa1e37d08cb074330e1d1d8b2
AndyChoi4495 <AndyChoi4495@github.com>
2026-01-27 11:58:53 -0500

    update api

--- Book-frontend/src/services/api.js
+++ Book-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.BACKEND_URL || 'http://localhost:5009',
+  baseURL: process.env.BACKEND_URL,
   headers: {
     'Content-Type': 'application/json',
   },
