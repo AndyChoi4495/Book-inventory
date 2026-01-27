cb3e564e001ee24e4f79a41eb1c7d2df3ded722a
AndyChoi4495 <AndyChoi4495@github.com>
2026-01-27 11:53:57 -0500

    add frontend cor origin

--- Book-backend/index.js
+++ Book-backend/index.js
@@ -22,7 +22,7 @@ app.use(
     origin: [
       'http://localhost:3000',
       'http://localhost:3001',
-      'https://', // 배포된 프론트엔드 주소
+      'https://book-inventory-frontend-plum.vercel.app', // 배포된 프론트엔드 주소
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
